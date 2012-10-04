c087f40080f1d81404d8d59270fd20c0c927f107
Jan Horacek (jahor/jhrcz) <jahor@jhr.cz>
2012-10-04 23:42:54 +0200

    new name from template name

--- one-fix-vm-names.sh
+++ one-fix-vm-names.sh
@@ -8,16 +8,26 @@ function dbq
   echo "$1" | $DB
 }
 
-dbq "select oid from vm_pool where state = 1 and name like 'one-%';" | while read oid
+#dbq "select oid from vm_pool where state = 1 or state = 3 and name like 'one-%';" | while read oid
+dbq "select oid from vm_pool where name like 'one-%';" | while read oid
 do
 	dbq "select name from vm_pool where oid = $oid;"| sed -e "s/^/DEBUG:/"
 	dbq "select body from vm_pool where oid = $oid;"| sed -e "s/^/DEBUG:/"
 
-	primary_volume_name=$(dbq "select body from vm_pool where oid = $oid;"  | xmlstarlet fo | xmlstarlet sel -t -v '//VM/TEMPLATE/DISK/IMAGE')
-	newname="${primary_volume_name// /_}"
+        #primary_volume_name=$(dbq "select body from vm_pool where oid = $oid;"  | xmlstarlet fo | xmlstarlet sel -t -v '//VM/TEMPLATE/DISK/IMAGE')
+	#newname="${primary_volume_name// /_}"
 
-	dbq "update vm_pool set body = replace( body, '<NAME>one-$oid</NAME>', '<NAME>$newname</NAME>' ) where oid like $oid;"
-	dbq "update vm_pool set name = replace( name, 'one-$oid', '$newname' ) where oid like $oid;"
+	templateid=$(dbq "select body from vm_pool where oid = $oid;"  | xmlstarlet fo | xmlstarlet sel -t -v '//VM/TEMPLATE/TEMPLATE_ID')
+	[ -n "$templateid" ] || continue
+	newname=$(dbq "select name from template_pool where oid = $templateid;")
+	newname="${newname// /_}"
+
+	if [ -n "$newname" ]
+	then
+		newname="tpl:$newname"
+		dbq "update vm_pool set body = replace( body, '<NAME>one-$oid</NAME>', '<NAME>$newname</NAME>' ) where oid like $oid;"
+		dbq "update vm_pool set name = replace( name, 'one-$oid', '$newname' ) where oid like $oid;"
+	fi
 
 done
 
